f3b488acf8c72527e255f63107a1d131a3e11588
Jerry Xiong <33706493+SmellyPickle@users.noreply.github.com>
2020-05-08 14:07:56 -0400

    Update train_supervised.py

--- train_supervised.py
+++ train_supervised.py
@@ -6,9 +6,11 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
+from bot_fast import FastBot
 from engine import Player, Game
 from entity import MoveCommand, SpawnShipCommand, ConstructDropoffCommand
 from viewer import Replayer
+
 
 def create_unet():
     frames_input = tf.keras.Input(shape=(128, 128, 4), name='frame_input')
@@ -18,13 +20,15 @@ def create_unet():
     d6 = layers.BatchNormalization()(d6)
     d6 = layers.Conv2D(64, kernel_size=3, padding='same', activation='relu', kernel_initializer='he_normal')(d6)
     d6 = layers.BatchNormalization()(d6)
-    d6 = layers.Conv2D(64, kernel_size=3, strides=2, padding='same', activation='relu', kernel_initializer='he_normal')(d6)
+    d6 = layers.Conv2D(64, kernel_size=3, strides=2, padding='same', activation='relu', kernel_initializer='he_normal')(
+        d6)
     d6 = layers.BatchNormalization()(d6)
     d6 = layers.Conv2D(64, kernel_size=3, padding='same', kernel_initializer='he_normal')(d6)
 
     d5 = layers.Activation('relu')(d6)
     d5 = layers.BatchNormalization()(d5)
-    d5 = layers.Conv2D(64, kernel_size=3, strides=2, padding='same', activation='relu', kernel_initializer='he_normal')(d5)
+    d5 = layers.Conv2D(64, kernel_size=3, strides=2, padding='same', activation='relu', kernel_initializer='he_normal')(
+        d5)
     d5 = layers.BatchNormalization()(d5)
     d5 = layers.Conv2D(64, kernel_size=3, padding='same', kernel_initializer='he_normal')(d5)
 
@@ -36,25 +40,29 @@ def create_unet():
 
     d3 = layers.Activation('relu')(d4)
     d3 = layers.BatchNormalization()(d3)
-    d3 = layers.Conv2D(64, kernel_size=3, strides=2, padding='same', activation='relu', kernel_initializer='he_normal')(d3)
+    d3 = layers.Conv2D(64, kernel_size=3, strides=2, padding='same', activation='relu', kernel_initializer='he_normal')(
+        d3)
     d3 = layers.BatchNormalization()(d3)
     d3 = layers.Conv2D(64, kernel_size=3, padding='same', kernel_initializer='he_normal')(d3)
 
     d2 = layers.Activation('relu')(d3)
     d2 = layers.BatchNormalization()(d2)
-    d2 = layers.Conv2D(64, kernel_size=3, strides=2, padding='same', activation='relu', kernel_initializer='he_normal')(d2)
+    d2 = layers.Conv2D(64, kernel_size=3, strides=2, padding='same', activation='relu', kernel_initializer='he_normal')(
+        d2)
     d2 = layers.BatchNormalization()(d2)
     d2 = layers.Conv2D(64, kernel_size=3, padding='same', kernel_initializer='he_normal')(d2)
 
     d1 = layers.Activation('relu')(d2)
     d1 = layers.BatchNormalization()(d1)
-    d1 = layers.Conv2D(64, kernel_size=3, strides=2, padding='same', activation='relu', kernel_initializer='he_normal')(d1)
+    d1 = layers.Conv2D(64, kernel_size=3, strides=2, padding='same', activation='relu', kernel_initializer='he_normal')(
+        d1)
     d1 = layers.BatchNormalization()(d1)
     d1 = layers.Conv2D(64, kernel_size=3, padding='same', kernel_initializer='he_normal')(d1)
 
     lf = layers.Activation('relu')(d1)
     lf = layers.BatchNormalization()(lf)
-    lf = layers.Conv2D(64, kernel_size=3, strides=2, padding='same', activation='relu', kernel_initializer='he_normal')(lf)
+    lf = layers.Conv2D(64, kernel_size=3, strides=2, padding='same', activation='relu', kernel_initializer='he_normal')(
+        lf)
     lf = layers.BatchNormalization()(lf)
     lf = layers.Conv2D(64, kernel_size=3, padding='same', kernel_initializer='he_normal')(lf)
     lf = layers.Reshape((64,))(lf)  # latent frame
@@ -145,26 +153,31 @@ class Dataset(tf.keras.utils.Sequence):
         batch_files = self.file_names[idx * self.batch_size: (idx + 1) * self.batch_size]
         loaded = [np.load(file) for file in batch_files]
         # {'moves_output': data['moves'], 'spawn_output': data['spawn']}
-        return [
-            np.array([data['frames'] for data in loaded]),
-            np.array([data['meta'] for data in loaded])
-        ], np.array([data['moves'] for data in loaded])
+        frames = np.array([data['frames'] for data in loaded])
+        frames[:, :, 0] = frames[:, :, 0] / 1000
+        return [frames, np.array([data['meta'] for data in loaded])], np.array([data['moves'] for data in loaded])
 
 
-folder = 'data/'
-files = [folder + f for f in os.listdir(folder)]
-train_dataset = Dataset(files, 16)
+if __name__ == '__main__':
+    folder = 'data/'
+    files = [folder + f for f in os.listdir(folder)]
+    train_dataset = Dataset(files, 16)
 
-val_folder = 'val/'
-files = [val_folder + f for f in os.listdir(val_folder)]
-val_dataset = Dataset(files, 16)
+    val_folder = 'val/'
+    files = [val_folder + f for f in os.listdir(val_folder)]
+    val_dataset = Dataset(files, 16)
 
-cp_callback = tf.keras.callbacks.ModelCheckpoint(
-    filepath='checkpoints/cp.ckpt',
-    verbose=1,
-    save_best_only=True,
-    save_weights_only=True
-)
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(
+        filepath='checkpoints/cp5000_{epoch:02d}.ckpt',
+        verbose=1,
+        save_weights_only=True
+    )
 
-model = create_unet()
-model.fit(train_dataset, epochs=10, callbacks=[cp_callback], validation_data=val_dataset)
+    es_callback = tf.keras.callbacks.EarlyStopping(
+        patience=3
+    )
+
+    my_model = create_unet()
+    my_model.load_weights(tf.train.latest_checkpoint('checkpoints/'))
+    my_model.fit(train_dataset, epochs=10, callbacks=[cp_callback, es_callback], validation_data=val_dataset)
+
