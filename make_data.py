71f488ae72823753ea684ec691eb06b63dab6560
Jerry Xiong <33706493+SmellyPickle@users.noreply.github.com>
2020-05-08 14:08:13 -0400

    Update make_data.py

--- make_data.py
+++ make_data.py
@@ -1,8 +1,17 @@
 from bot_fast import FastBot
 from engine import Game
 
-
-for i in range(20):
-    Game.run_game([FastBot(), FastBot()], map_width=32, save_name=f'data/game{i}')
-for i in range(5):
-    Game.run_game([FastBot(), FastBot()], map_width=32, save_name=f'val/game{i}')
+NUM_TRAIN = 300
+SPARSE_FREQ = 20
+EPS = 0.05
+NUM_VAL = 30
+for i in range(NUM_TRAIN):
+    if i % SPARSE_FREQ == 0:
+        print(f'Creating sparse training game {i+1:03}/{NUM_TRAIN}')
+        Game.run_game([FastBot(eps=EPS), FastBot(eps=EPS)], map_width=32, map_gen='sparse', save_name=f'data/game{i}', verbosity=0)
+    else:
+        print(f'Creating training game {i+1:03}/{NUM_TRAIN}')
+        Game.run_game([FastBot(eps=EPS), FastBot(eps=EPS)], map_width=32, save_name=f'data/game{i}', verbosity=0)
+for i in range(NUM_VAL):
+    print(f'Creating validation game {i+1:03}/{NUM_VAL}')
+    Game.run_game([FastBot(), FastBot()], map_width=32, save_name=f'val/game{i}', verbosity=0)
